1689564d786cf9874f869eb3a376d13f1410fb96
Shiyin Li <lsyin85@gmail.com>
2025-07-27 09:06:30 +0200

    change backend link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://taskmanager-backend-1-cz3z.onrender.com/'
 });
 
 export default api;
