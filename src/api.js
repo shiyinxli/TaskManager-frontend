c9e41dba5bf64765fd666e2ec12a63ea1a7b5c36
Shiyin Li <lsyin85@gmail.com>
2025-07-27 09:50:57 +0200

    update to /api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://taskmanager-backend-1-cz3z.onrender.com/',
+  baseURL: 'https://taskmanager-backend-1-cz3z.onrender.com/api',
 });
 
 export default api;
